88b04702d5375b97ee526a20d03e281284a30ec9
aymen <aymen.rahali@esprit.tn>
2023-04-01 23:16:16 +0100

    T2 added

---
+++ src/Entity/Participation.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ParticipationRepository::class)]
+class Participation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $nmbr_place_part = null;
+
+    #[ORM\Column]
+    private ?int $id_co = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $id_user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNmbrPlacePart(): ?int
+    {
+        return $this->nmbr_place_part;
+    }
+
+    public function setNmbrPlacePart(int $nmbr_place_part): self
+    {
+        $this->nmbr_place_part = $nmbr_place_part;
+
+        return $this;
+    }
+
+    public function getIdCo(): ?int
+    {
+        return $this->id_co;
+    }
+
+    public function setIdCo(int $id_co): self
+    {
+        $this->id_co = $id_co;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(?int $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+}
